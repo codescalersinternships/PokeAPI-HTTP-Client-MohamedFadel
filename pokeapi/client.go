2219d134c0dc14a97624e3aba9ebf742e4f32463
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-22 21:15:27 +0300

    feat: add NewClient constructor with configurable options

--- pokeapi/client.go
+++ pokeapi/client.go
@@ -17,3 +17,28 @@ type Client struct {
 	retryDelay    time.Duration
 	logger        *zap.Logger
 }
+
+/*
+NewClient creates a new PokeAPI client with the provided options.
+It uses default values for any option not explicitly set.
+*/
+func NewClient(options ...Option) *Client {
+	client := &Client{
+		baseURL:       "https://pokeapi.co/api/v2",
+		httpClient:    &http.Client{},
+		timeout:       30 * time.Second,
+		userAgent:     "PokeAPI-Go-Client/1.0",
+		retryAttempts: 3,
+		retryDelay:    time.Second,
+	}
+
+	for _, opt := range options {
+		opt(client)
+	}
+
+	if client.logger == nil {
+		client.logger, _ = zap.NewProduction()
+	}
+
+	return client
+}
