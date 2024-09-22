d95a6138c209ce574f7a132a5e0ca4dd3daaba1c
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-22 22:20:42 +0300

    feat: implement doRequest with retry mechanism using exponential backoff for HTTP requests

--- pokeapi/client.go
+++ pokeapi/client.go
@@ -1,9 +1,12 @@
 package pokeapi
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	"go.uber.org/zap"
 )
 
@@ -41,4 +44,51 @@ func NewClient(options ...Option) *Client {
 	}
 
 	return client
+}
+
+func (c *Client) doRequest(method, endpoint string, body io.Reader) (*http.Response, error) {
+	url := fmt.Sprintf("%s/%s", c.baseURL, endpoint)
+
+	var resp *http.Response
+	var err error
+
+	operation := func() error {
+		req, err := http.NewRequest(method, url, body)
+		if err != nil {
+			return fmt.Errorf("error creating request: %w", err)
+		}
+
+		req.Header.Set("User-Agent", c.userAgent)
+		req.Header.Set("Accept", "application/json")
+
+		c.logger.Info("Making request",
+			zap.String("method", method),
+			zap.String("url", url))
+
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			c.logger.Error("request failed",
+				zap.String("method", method),
+				zap.String("url", url),
+				zap.Error(err))
+
+			return fmt.Errorf("error making request: %w", err)
+		}
+
+		if resp.StatusCode >= 500 {
+			return fmt.Errorf("server error: %s", resp.Status)
+		}
+
+		return nil
+	}
+
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = c.timeout
+
+	err = backoff.Retry(operation, b)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
