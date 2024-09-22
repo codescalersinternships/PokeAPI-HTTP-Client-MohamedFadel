3a3affc4bf5ac606e0f8178b7ab4bc091b35f718
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-22 22:32:04 +0300

    feat: add Pokemon model with basic attributes

---
+++ pokeapi/models.go
@@ -0,0 +1,10 @@
+package pokeapi
+
+type Pokemon struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+}
+
